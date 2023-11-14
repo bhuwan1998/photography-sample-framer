cd426798df31c7c78f677ac1143504573f2af248
Bhuwan Bansal <bhuwan.bansal@outlook.com>
2023-11-14 18:15:26 +1100

    Setting up the website pages

--- src/components/AnimRoutes.js
+++ src/components/AnimRoutes.js
@@ -1,7 +1,23 @@
 import React from 'react';
+// import pages 
+import About from '../pages/About';
+import Contact from '../pages/Contact'; 
+import Home from '../pages/Home';
+import Portfolio from '../pages/Portfolio';
+
+// import router & useLocation hook 
+import { Routes, Route, useLocation} from 'react-router-dom';
+
+
 
 const AnimRoutes = () => {
-  return <div>AnimRoutes</div>;
+
+  return <Routes>
+    <Route path="/" element={<Home />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/contact" element={<Contact />} />
+    <Route path="/portfolio" element={<Portfolio />} />
+  </Routes>
 };
 
 export default AnimRoutes;
